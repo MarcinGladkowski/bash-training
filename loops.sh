ffa2c3209ea041abf3008eab6dbe495752ae5d59
Martin89PL <marcingladkowski@gmail.com>
2020-05-26 22:00:13 +0200

    add loops exercise

---
+++ loops.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Bash like for
+#
+# for var in list ; do
+#   _do something_
+# done
+#
+# or another 
+#  
+# for var in list
+# do
+#   _do something_
+# done
+
+
+for x in one two three ; do
+    echo "This is $x"
+done
